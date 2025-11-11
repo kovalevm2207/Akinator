b2405632e656f814db02efdcaee98a596e58a544
kovalevm2207 <kovalevm220741@gmail.com>
2025-11-11 17:51:44 +0300

    сделал функцию считывания в буффер ответа пользователя

--- AkinatorMain.cpp
+++ AkinatorMain.cpp
@@ -1,24 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "TreeMemStruct/Tree.h"
 #include "color_print.h"
 
 const int SECOND = 1e6;
+const int START_LENGTH = 1 << 2;
 
 typedef enum
 {
     AKINATOR_OK = 1 << 0
 } AkinatorErr_t;
 
+typedef enum
+{
+    DEFINITION = 1,
+    COMPARISON = 2,
+    UPDATE     = 3,
+    GUESSING   = 4,
+    END        = 5
+} AkinatorMode_t;
+
+char* GetUserAns(char* buffer);
 int HelloUser(void);
+
+#ifdef DEBUG
+    #define ON_DEBUG(func) func
+#else
+    #define ON_DEBUG(func)
+#endif
 
 int main()
 {
-    HelloUser();
+    if (HelloUser() <= 0) { printf(RED_COLOR "FATAL ERROR IN FUNCTION int HelloUser(void)\n" RESET); return 0;}
+
+    AkinatorMode_t AM = END;
+    do
+    {
+        char* UserAns = (char*) calloc(START_LENGTH, sizeof(char));
+        if (UserAns == NULL) {printf(RED_COLOR "MEMORY ALLOCATION ERR\n" RESET); return 1;}
+
+        UserAns = GetUserAns(UserAns);
+        if (UserAns == NULL) {printf(RED_COLOR "ERR IN FUNC char* GetUserAns(char* buffer)\n"); free(UserAns); UserAns = NULL; return 1;}
+
+        
+
+        free(UserAns);
+        UserAns = NULL;
+    } while (AM != END);
 
     return 0;
+}
+
+
+char* GetUserAns(char* buffer)
+{
+    long unsigned int CUR_LENGTH = START_LENGTH;
+    long unsigned int LAST_LENGTH = 0;
+
+    while (1)
+    {
+    if (fgets(buffer + LAST_LENGTH, (int) (CUR_LENGTH - LAST_LENGTH), stdin) == NULL)
+    {
+        printf("3");
+        printf(RED_COLOR "FGETS USER ANSWER ERR\n" RESET); return NULL;
+    }
+
+    ON_DEBUG(printf("Read: '");
+    for(long unsigned int i = 0; i < CUR_LENGTH; i++) {
+        switch(buffer[i])
+        {
+            case '\0': printf("\\0"); break;
+            case '\n': printf("\\n"); break;
+            default: printf("%c", buffer[i]); break;
+        }
+    }
+    printf("'\n");)
+
+    char* slash_n_pos = strchr(buffer, '\n');
+
+    ON_DEBUG(printf("strchr result: %p\n", slash_n_pos);)
+
+    if (slash_n_pos == NULL)
+    {
+        char* Newbuffer = (char*) realloc(buffer, CUR_LENGTH * 2);
+        if (Newbuffer == NULL) {printf(RED_COLOR "MEMORY ALLOCATION ERR\n" RESET); return NULL;}
+
+        buffer = Newbuffer;
+        memset(buffer + CUR_LENGTH, '\0', sizeof(char) * CUR_LENGTH);
+        LAST_LENGTH = CUR_LENGTH - 1;
+        CUR_LENGTH *= 2;
+
+        continue;
+    }
+    else break;
+    }
+
+    ON_DEBUG(fputs(buffer, stdout);)
+    return buffer;
 }
 
 int HelloUser(void)
